3f758e8255ac7f62561ead471a880bc8f3537adb
theejer <leejinrae@gmail.com>
2025-10-05 12:41:35 +0800

    Added db connection, axios and bootstrap

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,25 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres.okkqvwmdmnwadkokzynz',
+  host: 'aws-1-ap-southeast-1.pooler.supabase.com',
+  database: 'SMU Events Hub',
+  password: 'group7loveswad2',
+  port: 5432, 
+});
+
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Database connection error', err);
+  } else {
+    console.log('Database connected at:', res.rows[0].now);
+  }
+});
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios'
 import App from './App.vue'
 
 createApp(App).mount('#app')
