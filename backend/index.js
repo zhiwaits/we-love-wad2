63b0fee743e0a2377d6852a72bfbdfa7f9fdc12a
Terry Yeo <Terryyeo88@gmail.com>
2025-11-06 00:12:30 +0800

    message

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ const corsOptions = {
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'token']
 };
 
 app.use(cors(corsOptions));
