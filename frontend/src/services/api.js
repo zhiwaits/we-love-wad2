596be2cae5f21f36540fcde4ef665d210660c55b
Terry Yeo <Terryyeo88@gmail.com>
2025-11-06 00:35:24 +0800

    message

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 
 import axios from 'axios';
-
+const backendURL = import.meta.env.BACKENDURL
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: BACKENDURL,
   headers: {
     'Content-Type': 'application/json',
   },
